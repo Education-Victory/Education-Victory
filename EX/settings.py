bff631d7f8b4c1745bcabb4626a509260a1d5f1e
zhiyiqi <yolandazik@qq.com>
2023-01-12 01:42:21 -0500

    fix: register quiz app

--- EX/settings.py
+++ EX/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'quiz.apps.QuizConfig'
 ]
 
 MIDDLEWARE = [
@@ -47,7 +48,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'quiz.apps.QuizConfig'
+    
 ]
 
 ROOT_URLCONF = 'EX.urls'
