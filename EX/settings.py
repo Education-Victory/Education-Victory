ad04cd3722d1d1dbb2c1b49a522d50c38b8ba4d8
zhiyiqi <yolandazik@qq.com>
2023-01-12 01:31:29 -0500

    register quiz app

--- EX/settings.py
+++ EX/settings.py
@@ -47,6 +47,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'quiz.apps.QuizConfig'
 ]
 
 ROOT_URLCONF = 'EX.urls'
