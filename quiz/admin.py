2af5d85193531ace54942869fe4a9a397fa25ee4
zhiyiqi <yolandazik@qq.com>
2023-01-12 02:18:27 -0500

    register created models

--- quiz/admin.py
+++ quiz/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Quiz, QuizQuestion, QuestionOption, QuizSubmission
+admin.site.register(Quiz)
+admin.site.register(QuizQuestion)
+admin.site.register(QuestionOption)
+admin.site.register(QuizSubmission)
+
