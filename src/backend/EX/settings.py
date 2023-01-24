9a9f0e7505006cdaaaa9ba3454053a9102f2d2d9
Yida Zou <1306511984@qq.com>
2023-01-23 22:19:27 -0800

    add user model in account

--- src/backend/EX/settings.py
+++ src/backend/EX/settings.py
@@ -134,7 +134,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-#AUTH_USER_MODEL = 'accounts.CustomUser'
+AUTH_USER_MODEL = 'accounts.CustomUser'
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
