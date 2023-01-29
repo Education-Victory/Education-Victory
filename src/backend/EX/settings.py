23df005df165f638a0a74545529f49dc69e2dfc7
Windson Yang <wiwindson@outlook.com>
2023-01-29 17:11:42 +0800

    add html files for public app and set up the default home page

--- src/backend/EX/settings.py
+++ src/backend/EX/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+DEFAULT_DIR = Path(__file__).resolve().parent
 
 
 # Quick-start development settings - unsuitable for production
@@ -62,7 +63,7 @@ ROOT_URLCONF = 'EX.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')], # import the templates folder
+        'DIRS': [os.path.join(DEFAULT_DIR, 'templates')], # import the templates folder
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
