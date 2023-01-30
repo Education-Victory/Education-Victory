7febb6d62070014028497fe4bf7a46aabfba5ed2
Windson Yang <wiwindson@outlook.com>
2023-01-30 15:53:03 +0800

    add and setup STATIC_DIR

--- src/backend/EX/settings.py
+++ src/backend/EX/settings.py
@@ -14,6 +14,8 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+DEFAULT_DIR = Path(__file__).resolve().parent
+STATIC_DIR = Path(__file__).resolve().parent.parent.parent
 
 
 # Quick-start development settings - unsuitable for production
@@ -62,7 +64,7 @@ ROOT_URLCONF = 'EX.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')], # import the templates folder
+        'DIRS': [os.path.join(DEFAULT_DIR, 'templates')], # import the templates folder
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -126,11 +128,14 @@ USE_I18N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    STATIC_DIR / "static",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
