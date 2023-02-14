142d07857f3e08678cd18c13ab8a2e2ecb5f3145
Wenbo <wenbojijack@gmail.com>
2023-02-13 18:07:27 -0800

    vue files

---
+++ src/frontend/education-v/src/views/HomeView.vue
@@ -0,0 +1,11 @@
+<script setup>
+import Header from '../components/Header.vue'
+import Landing from '../components/Landing.vue'
+import Footer from '../components/Footer.vue'
+</script>
+
+<template>
+  <Header />
+  <Landing />
+  <Footer />
+</template>
