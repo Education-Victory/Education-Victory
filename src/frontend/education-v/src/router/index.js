fd055d6029bf480bd0f6fb126ac9f57cc1948180
Wenbo <wenbojijack@gmail.com>
2023-02-13 17:53:21 -0800

    static files

---
+++ src/frontend/education-v/src/router/index.js
@@ -0,0 +1,15 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    {
+      path: '/',
+      name: 'Home',
+      component: HomeView
+    },
+  ]
+})
+
+export default router
