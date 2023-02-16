7afa212e0a98e589f22f171d03b52d8bd5aa9b89
Windson Yang <wiwindson@outlook.com>
2023-02-16 17:18:46 +0800

    Add ROOT to project ENV

--- src/backend/public/views.py
+++ src/backend/public/views.py
@@ -1,4 +1,8 @@
+import ENV
 from django.shortcuts import render
 
 def home(request):
-    return render(request, 'public/home.html')
+    return render(request, 'public/home.html', {'root': ENV.ROOT})
+
+def question(request):
+    return render(request, 'public/question.html', {'root': ENV.ROOT})
