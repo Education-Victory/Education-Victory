cdb1b003c44ce2a49ac7c6779d38f2be1b8e2d55
Windson Yang <wiwindson@outlook.com>
2023-02-18 11:00:12 +0800

    Create question page and API

--- src/backend/EX/urls.py
+++ src/backend/EX/urls.py
@@ -44,4 +44,5 @@ urlpatterns = [
     path('accounts/', include('allauth.urls')),
     path('api/', include((router.urls, 'app_name'))),
     path('', public_views.home, name='home'),
+    path('question/', public_views.question, name='question'),
 ]
