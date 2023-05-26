03e803863b4e860f78529ae774f5e43eb0d2bdad
Yi Wei <weiyi0124@gmail.com>
2023-05-25 21:45:31 -0400

    init evaluation page

--- src/backend/EX/urls.py
+++ src/backend/EX/urls.py
@@ -48,4 +48,5 @@ urlpatterns = [
     path('', public_views.home, name='home'),
     path('question/<str:question_name>/<str:category_name>/', public_views.question_detail, name='question_detail'),
     path('question/', public_views.question, name='question'),
+    path('evaluate/', public_views.evaluate, name='evaluate'),
 ]
--- src/backend/public/views.py
+++ src/backend/public/views.py
@@ -7,8 +7,9 @@ def home(request):
 def question(request):
     return render(request, 'public/question.html', {'root': ENV.ROOT})
 
-
 def question_detail(request, question_name, category_name):
     return render(request, 'public/question_detail.html',
         {'root': ENV.ROOT, 'question_name': question_name, 'category_name': category_name})
 
+def evaluate(request):
+    return render(request, 'public/evaluate.html', {'root': ENV.ROOT})
