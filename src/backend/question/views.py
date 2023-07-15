5ca9f97bba2d6b5430e161878bff618a39d3183c
Kejie He <kjhe217@gmail.com>
2023-07-14 23:27:33 -0700

    update driver method for the suggestion algorithm

--- src/backend/question/views.py
+++ src/backend/question/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
 from django.db.models import F
-from rest_framework import viewsets
+from requests import Response
+from rest_framework import viewsets, status
 from common.utils import cal_score
 from .models import Question, Solution, Keypoint, Category
 from .serializers import QuestionSerializer, SolutionSerializer, KeypointSerializer, CategorySerializer
+from rest_framework.decorators import action
+from common.models import UserSubmission
+from common.utils import cal_score
+from datetime import datetime, timedelta
+
 
 class CategoryViewSet(viewsets.ModelViewSet):
     serializer_class = CategorySerializer
@@ -11,6 +17,7 @@ class CategoryViewSet(viewsets.ModelViewSet):
     def get_queryset(self):
         queryset = Category.objects.all()
         return queryset
+
 
 class QuestionViewSet(viewsets.ModelViewSet):
     serializer_class = QuestionSerializer
@@ -22,6 +29,7 @@ class QuestionViewSet(viewsets.ModelViewSet):
         if category is not None:
             queryset = queryset.filter(category=category)
         return queryset
+
 
 class SolutionViewSet(viewsets.ModelViewSet):
     serializer_class = SolutionSerializer
@@ -52,22 +60,56 @@ class SolutionViewSet(viewsets.ModelViewSet):
         # type: 1. review_mode 2. submit more than 3 times
         # group: Data structures / Algorithms / Technique / Others
         # count: total count of solution should return
+
+        # Parse the user from request
         user = request.user
-        submission = UserSubmission.objects.filter(...)
+        if not user:
+            # Handle unauthorized request here
+            # TODO: Refactor the error message in serializer
+            return Response("User not logged in",
+                            status=status.HTTP_401_UNAUTHORIZED)
+
+        # Get all submissions of current user
+        submission = UserSubmission.objects.filter(user_id=user.id)
+
+        # All solutions before filtering
+        queryset = Solution.objects.all()
         group = self.request.query_params.get('group', None)
         count = self.request.query_params.get('count', 0)
         # Step 1: Implement filter
         if group is not None:
-            queryset = queryset.filter(...)
+            queryset = queryset.filter(group=group)
+        # TODO: define type enums as constants
         if type == 1:
-            queryset = queryset.filter(...)
+            # queryset = queryset.filter(...)
+            pass
         # Step 2: Calculate solution score
         res = []
-        for query in queryset:
+
+        # Calculate the date num_days_ago days ago
+        num_days_ago = 14
+        user_freq_start_date = datetime.now().date() - timedelta(days=num_days_ago)
+        # Count the number of user submissions within the last 14 days
+        submission_count = submission.filter(
+            created_at__gte=user_freq_start_date).count()
+        submission_frequency = submission_count / num_days_ago
+        is_review_mode = self.request.query_params.get('review', False)
+
+        for this_solution in queryset:
+            # Filter out user submissions for this particular solution
+            this_submission_query = submission.filter(
+                solution_id=this_solution.id).order_by('-created_at')
             # cal_score is a pure function have no database operation in it
-            score = utils.cal_score(user.ability, query.ability, submission...)
-            res.append(score)
+            score = cal_score(
+                user.ability, this_solution.ability, this_submission_query, metadata={
+                    "submission_frequency": submission_frequency,
+                    "is_review_mode": is_review_mode
+                })
+            # Add item for sorting
+            res.append([score, this_solution])
         res.sort(reverse=True)
+        # Retrieve back the solution obj
+        res = [item[1] for item in res]
         serializer = SolutionSerializer(res[:count], many=True)
         return Response(serializer.data)
 
