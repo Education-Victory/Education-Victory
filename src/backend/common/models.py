cc868d3a1ad4e74a294fd01c22d0843c5f100189
Windson Yang <wiwindson@outlook.com>
2023-07-19 11:38:44 +0800

    Modify the model according to product requirements

--- src/backend/common/models.py
+++ src/backend/common/models.py
@@ -4,14 +4,22 @@ from django.contrib.auth.models import AbstractUser
 from django.conf import settings
 from question.models import Solution
 
+
 class User(AbstractUser):
-    avatar = models.URLField(max_length=1024, blank=True, help_text="URL for avatar")
+    name = models.CharField(max_length=100)
+    avatar = models.URLField(
+        max_length=1024, blank=True, help_text="URL for avatar")
+    info = models.JSONField(help_text='data: [1, 2, 0]')
+    ability = models.JSONField(help_text='data: [10, 20, 10]')
 
 
 class UserSubmission(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='submission_user')
-    solution = models.ForeignKey(Solution, on_delete=models.CASCADE, related_name='submission_solution')
-    content = models.JSONField()
+    user_id = models.BigIntegerField(
+        help_text='user_id (settings.AUTH_USER_MODEL.id)')
+    question_id = models.BigIntegerField(help_text='question id (Question.id)')
+    solution_id = models.BigIntegerField(help_text='solution_id (Solution.id)')
+    hash_code = models.CharField(max_length=200)
+    code = models.JSONField(blank=True, help_text='code in submission')
+    completeness = models.JSONField()
     created_at = models.DateTimeField(default=timezone.now)
     updated_at = models.DateTimeField(auto_now=True)
-
--- src/backend/question/models.py
+++ src/backend/question/models.py
@@ -1,72 +1,61 @@
+from uu import Error
 from django.db import models
 from django.utils import timezone
 from django.conf import settings
 
+
 class Category(models.Model):
-    name = models.CharField(max_length=30, help_text='category of solution')
-    weight = models.IntegerField(default=1, help_text='weight of the category')
+    name = models.CharField(max_length=30, help_text='category of question/solution')
+    weight = models.IntegerField(default=1)
     created_at = models.DateTimeField(default=timezone.now)
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.name
+
 
 class Question(models.Model):
-    class QuestionTypeChoice(models.TextChoices):
-        OTHERS = 'OT', 'Others'
-        MULTIPLE_CHOICE = 'MC', 'Multiple Choice'
-        SHORT_ANSWER = 'SA', 'Short Answer'
-        TRUE_O_FALSE = 'TF', 'True / False'
-
-    name = models.CharField(max_length=100, blank=True, help_text='question name')
-    description = models.JSONField(help_text='description of question')
-    category = models.ManyToManyField(Category)
-    type = models.CharField(max_length=2, choices=QuestionTypeChoice.choices, help_text='type of question')
-    upvote = models.IntegerField(default=1, help_text='upvote of the question')
-    downvote = models.IntegerField(default=1, help_text='downvote of the question')
+    name = models.CharField(max_length=100)
+    description = models.JSONField(blank=True)
+    ide_description = models.JSONField(blank=True, help_text='default description/code in the IDE')
+    category_id_list = models.ManyToManyField(
+        Category, help_text='category list related to question based on its solution')
+    upvote = models.IntegerField(default=1)
+    downvote = models.IntegerField(default=1)
     publish = models.BooleanField(default=1, help_text='publish the question or not')
-    URL = models.URLField(max_length=20, blank=True, help_text='URL of question')
+    URL = models.URLField(max_length=20, blank=True)
     created_at = models.DateTimeField(default=timezone.now)
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.name
 
-class Keypoint(models.Model):
-    name = models.CharField(max_length=50, help_text='name of the keypoint')
-    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='keypoint', blank=True, null=True)
-    difficulty = models.IntegerField(default=1, help_text='difficulty of a keypoint')
-    requirements = models.CharField(max_length=100, blank=True, help_text='requirements of keypoint')
+
+class TestCase(models.Model):
+    question_id = models.ForeignKey(
+        Question, on_delete=models.CASCADE, related_name='testcase_question')
+    input = models.JSONField(help_text='JSON: {array: [1,2,3], target: 10}')
+    output = models.JSONField(help_text='JSON: {result: [0,1]}')
     created_at = models.DateTimeField(default=timezone.now)
     updated_at = models.DateTimeField(auto_now=True)
 
-    def __str__(self):
-        return f'{self.name}'
 
 class Solution(models.Model):
-    name = models.CharField(max_length=100, blank=True, help_text='solution name')
-    question = models.ForeignKey(Question, on_delete=models.CASCADE, related_name='solution_question')
-    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='solution_category')
-    answer = models.JSONField(help_text='detailed solution')
-    keypoint = models.ManyToManyField(Keypoint)
+    class Type(models.TextChoices):
+        CODING = 'CO', 'Coding'
+        CHOICE = 'CH', 'Choice'
+
+    name = models.CharField(max_length=100)
+    type = models.CharField(
+        max_length=2, choices=Type.choices, help_text='solution type')
+    description = models.JSONField(help_text='text in the IDE')
+    question_id = models.ForeignKey(
+        Question, on_delete=models.CASCADE, related_name='solution_question')
+    category_id_list = models.ManyToManyField(Category)
+    ability = models.JSONField()
     resources = models.JSONField(help_text='resources of question')
     created_at = models.DateTimeField(default=timezone.now)
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return f'{self.name} - {self.question}'
-
-class UserKeypointScore(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    keypoint = models.ForeignKey(Keypoint, on_delete=models.CASCADE)
-    score = models.IntegerField(default=0)
-    created_at = models.DateTimeField(default=timezone.now)
-    updated_at = models.DateTimeField(auto_now=True)
-
-class UserSubmission(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    solution = models.ForeignKey(Solution, on_delete=models.CASCADE)
-    keypoint = models.JSONField(blank=True, help_text='list of completed keypoint id')
-    content = models.JSONField(blank=True)
-    created_at = models.DateTimeField(default=timezone.now)
-    updated_at = models.DateTimeField(auto_now=True)
