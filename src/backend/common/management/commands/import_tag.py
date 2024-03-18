d860a27cdd94538491a5620b8c89cb4831c09a5b
Windson yang <wiwindson@outlook.com>
2024-03-18 11:41:50 +0800

    clean up code

--- src/backend/common/management/commands/import_tag.py
+++ src/backend/common/management/commands/import_tag.py
@@ -1,33 +1,41 @@
 import json
 import os
 from django.core.management.base import BaseCommand
-from django.utils import timezone
 from question.models import Tag  # Ensure this is the correct import path
 
 class Command(BaseCommand):
     help = 'Imports default tags into the database'
 
+    def add_arguments(self, parser):
+        parser.add_argument('data_file', type=str, help='The JSON file with the default tags')
+
     def handle(self, *args, **options):
-        data_file_path = os.path.join(os.path.dirname(__file__), 'default_tag.json')
+        data_file_path = options['data_file']
+
+        if not os.path.isfile(data_file_path):
+            self.stdout.write(self.style.ERROR(f'File does not exist: {data_file_path}'))
+            return
 
         # Load data from JSON file
         with open(data_file_path, 'r') as file:
             data = json.load(file)
 
-        for category, tags in data.items():
+        for category_name, tags in data.items():
             for tag_info in tags:
                 # Extracting details for each tag
                 name = tag_info['name']
-                category = tag_info.get('category', 'General')
+                group = tag_info['group']
+                short_name = tag_info['short_name']
                 frequency = tag_info.get('frequency', 1)
                 difficulty = tag_info.get('difficulty', 1)
 
                 # Creating or updating the tag
-                tag, created = Tag.objects.get_or_create(
+                tag, created = Tag.objects.update_or_create(
                     name=name,
                     defaults={
-                        'created_at': timezone.now(),
-                        'category': category,
+                        'short_name': short_name,
+                        'group': group,
+                        'category': category_name,
                         'frequency': frequency,
                         'difficulty': difficulty
                     }
@@ -36,9 +44,5 @@ class Command(BaseCommand):
                 if created:
                     self.stdout.write(self.style.SUCCESS(f'Successfully added tag "{name}"'))
                 else:
-                    # If the tag exists, you might want to update its fields
-                    tag.category = category
-                    tag.frequency = frequency
-                    tag.difficulty = difficulty
-                    tag.save()
                     self.stdout.write(f'Tag "{name}" already exists and was updated.')
+
