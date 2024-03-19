a7a16e679738da1ad5da636e86b7f373b0ade1d2
Windson yang <wiwindson@outlook.com>
2024-03-19 16:48:36 +0800

    udpate recommend algorithm

--- src/backend/problem/views.py
+++ src/backend/problem/views.py
@@ -2,13 +2,15 @@ from math import ceil
 from django.conf import settings
 from django.shortcuts import render
 from django.http import JsonResponse
-from django.db.models import F
+from django.db.models import F, Subquery
 from django.dispatch import receiver
 from rest_framework.views import APIView
 from rest_framework.viewsets import ModelViewSet
-from .models import Problem
+from rest_framework.response import Response
+from .models import Problem, TagProblem
 from .serializers import ProblemSerializer
 from question.models import Question
+from common.models import UserAbility
 
 
 def problem(request, name):
@@ -17,12 +19,18 @@ def problem(request, name):
 
 
 def get_weak_tag(user, category, num=3):
+    # First, get a queryset of tag IDs that exist in TagProblem.
+    tags_in_tagproblem = TagProblem.objects.values_list('tag_id', flat=True).distinct()
+
+    # Now, filter UserAbility objects for the given user and category,
+    # ensuring that the tag is one of those present in TagProblem.
     weak_tag_ability = UserAbility.objects.filter(
         user=user,
-        tag__category=category
+        tag__category=category,
+        tag_id__in=Subquery(tags_in_tagproblem)
     ).order_by('ability_score')[:num]
 
-    return weak_tag_ability
+    return [user_ability.tag for user_ability in weak_tag_ability]
 
 
 class ProblemViewSet(ModelViewSet):
@@ -43,31 +51,47 @@ class RecommendProblemView(APIView):
         user = request.user
         category = request.query_params.get('category', 'recommend')
         user_info = getattr(user, 'info', {})
-        user_settings = user_info.get('settings', {'algorithm': 50, 'system_design': 30, 'behavioral': 20})
-        total_problems = 6  # Total number of problems to return
+        user_settings = user_info.get('settings', {'algorithm': 50, 'system-design': 30, 'behavioral': 20})
+        total_problems = 10  # Total number of problems to return
 
         problems = []
-
         if category == 'recommend':
-            # Calculate the number of problems to fetch for each category based on the percentages
             for cat, percentage in user_settings.items():
                 num_problems = ceil(total_problems * (percentage / 100))
-                weak_tag = get_weak_tag(user, cat, num=1)
-                # Fetch problems for each category based on the weak tag and the calculated number
+                weak_tags = get_weak_tag(user, cat, num=3)  # Fetch more tags to have options
+                collected_problems = 0
+
+                for tag in weak_tags:
+                    if collected_problems >= num_problems:
+                        break  # Stop if we have collected enough problems
+
+                    category_problems = Problem.objects.filter(
+                        tags=tag,  # Filter by current tag
+                        category=cat
+                    ).annotate(
+                        difficulty=F('tagproblem__difficulty')
+                    ).order_by('-difficulty')[:num_problems - collected_problems]
+
+                    problems.extend(list(category_problems))
+                    collected_problems += len(category_problems)
+        else:
+            # Similar logic for a specific category, fetching more tags
+            weak_tags = get_weak_tag(user, category, num=3)  # Fetch more tags
+            collected_problems = 0
+
+            for tag in weak_tags:
+                if collected_problems >= total_problems:
+                    break
+
                 category_problems = Problem.objects.filter(
-                    tagproblem__tag=weak_tag,
-                    category=cat
+                    tags=tag,
+                    category=category
                 ).annotate(
                     difficulty=F('tagproblem__difficulty')
-                ).order_by('-difficulty')[:num_problems]
+                ).order_by('-difficulty')[:total_problems - collected_problems]
+
                 problems.extend(list(category_problems))
-        else:
-            # Fetch problems for the explicitly requested category
-            problems = Problem.objects.filter(
-                category=category
-            ).annotate(
-                difficulty=F('tagproblem__difficulty')
-            ).order_by('-difficulty')[:total_problems]
+                collected_problems += len(category_problems)
 
         # Serialize the problem instances
         serializer = ProblemSerializer(problems, many=True)
