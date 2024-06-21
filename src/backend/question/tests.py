7597e61be98788bed9636988046ba137520e50bb
Windson yang <wiwindson@outlook.com>
2024-06-21 17:12:35 +0800

    remove invalid test

--- src/backend/question/tests.py
+++ src/backend/question/tests.py
@@ -1,12 +1,1 @@
 from django.test import TestCase
-
-from .models import Category
-
-class CategoryTestCase(TestCase):
-    def setUp(self):
-        Category.objects.create(name='Binary Search', weight=1)
-
-    def test_model_can_create_a_category(self):
-        cate = Category.objects.get(name='Binary Search')
-        self.assertEqual(cate.name, 'Binary Search')
-        self.assertEqual(cate.weight, 1)
